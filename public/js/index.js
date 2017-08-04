07008ca3b6eb90764da6abbb937fb07af40ff9ba
Douglas Marquardt <douglasmarquardt@gmail.com>
2017-08-04 12:01:36 -0300

    Emit newMessage and createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,6 @@ var socket = io();
 socket.on('connect', function () {
   console.log('Connected to server');
 
-  socket.emit('createMessage', {
-    from: 'jen',
-    text: 'Hey, This is Jen.'
-  });
 });
 
 socket.on('disconnect', function () {
