dce6cfc95c14f249132c3233df3099aab4e10e7f
Douglas Marquardt <douglasmarquardt@gmail.com>
2017-08-08 19:41:24 -0300

    Fix require users.js typo

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const socketIO = require('socket.io');
 
 const {generateMessage, generateLocationMessage} = require('./utils/message');
 const {isRealString} = require('./utils/validation');
-const {Users} = require('./utils/Users');
+const {Users} = require('./utils/users');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
